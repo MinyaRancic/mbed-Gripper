f980b6d054edc1d397bd6124a69a0f632b9bb70c
whatsGravity <rancic.minya@gmail.com>
2016-07-25 11:36:27 -0400

    get and set methods written
    all basic functionality is complete

--- mbedGripper.m
+++ mbedGripper.m
@@ -25,13 +25,51 @@ classdef mbedGripper < matlab.mixin.SetGet
     methods
         function Angle = get.Angle(obj)
             flushinput(obj.mbedSerial);
-            fprintf(obj.mbedSerial, 'angl', 'async');
-            Angle = fscanf(obj.mbedSerial);
+            flushoutput(obj.mbedSerial);
+            fprintf(obj.mbedSerial, '1,0,0\n', 'async');
+            Angle = str2double(fscanf(obj.mbedSerial));
         end
         
         function set.Angle(obj, value)
             flushoutput(obj.mbedSerial);
-            fprintf(obj.mbedSerial, '%f', value);s
+            flushinput(obj.mbedSerial);
+            fprintf(obj.mbedSerial, '0,0,%f\n', value, 'async');
+        end
+        
+        function kP = get.kP(obj)
+            flushoutput(obj.mbedSerial);
+            flushinput(obj.mbedSerial);
+            fprintf(obj.mbedSerial, '1,1,0\n', 'async');
+        end
+        
+        function set.kP(obj, value)
+            flushoutput(obj.mebedSerial);
+            flushinput(obj.mbedSerial);
+            fprintf(obj.mbedSerial, '0,1,%f\n', value, 'async');
+        end
+        
+        function kI = get.kI(obj)
+            flushoutput(obj.mbedSerial);
+            flushinput(obj.mbedSerial);
+            fprintf(obj.mbedSerial, '1,2,0\n', 'async');
+        end
+        
+        function set.kI(obj, value)
+            flushoutput(obj.mebedSerial);
+            flushinput(obj.mbedSerial);
+            fprintf(obj.mbedSerial, '0,2,%f\n', value, 'async');
+        end
+        
+        function kD = get.kD(obj)
+            flushoutput(obj.mbedSerial);
+            flushinput(obj.mbedSerial);
+            fprintf(obj.mbedSerial, '1,3,0\n', 'async');
+        end
+        
+        function set.kD(obj, value)
+            flushoutput(obj.mebedSerial);
+            flushinput(obj.mbedSerial);
+            fprintf(obj.mbedSerial, '0,3,%f\n', value, 'async');
         end
     end
     
